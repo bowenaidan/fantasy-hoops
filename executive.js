1fd0bb655dc061fd5ab4aa1faed7d66ea6c5642c
Aidan Bowen <95006701+bowenaidan@users.noreply.github.com>
2025-11-08 16:18:19 -0600

    change to run today's games

--- executive.js
+++ executive.js
@@ -1,6 +1,6 @@
 function updateRunner() {
   const d = new Date();
-  d.setDate(d.getDate() - 1);
+  d.setDate(d.getDate());
   const yyyy = d.getFullYear();
   const mm = String(d.getMonth() + 1).padStart(2, '0');
   const dd = String(d.getDate()).padStart(2, '0');
