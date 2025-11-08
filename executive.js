4024c8f900e7cd23d0e3dfbebd8ce8e87ab84e9f
Aidan Bowen <95006701+bowenaidan@users.noreply.github.com>
2025-11-08 15:40:34 -0600

    add apps script files

---
+++ executive.js
@@ -0,0 +1,14 @@
+function updateRunner() {
+  const d = new Date();
+  d.setDate(d.getDate() - 1);
+  const yyyy = d.getFullYear();
+  const mm = String(d.getMonth() + 1).padStart(2, '0');
+  const dd = String(d.getDate()).padStart(2, '0');
+  isoDate = `${yyyy}/${mm}/${dd}`;
+  dailySync(isoDate);
+  // dailySync("2025/11/03");
+  // dailySync("2025/11/04");
+  // dailySync("2025/11/05");
+  // dailySync("2025/11/06");
+  // dailySync("2025/11/07");
+}
