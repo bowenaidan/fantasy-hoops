16afbdded81eee5ef6e4c7f288fd91333a7d72a0
Aidan Bowen <95006701+bowenaidan@users.noreply.github.com>
2025-11-17 20:55:21 -0600

    cleanup

--- executive.js
+++ executive.js
@@ -6,9 +6,4 @@ function updateRunner() {
   const dd = String(d.getDate()).padStart(2, '0');
   isoDate = `${yyyy}/${mm}/${dd}`;
   dailySync(isoDate);
-  // dailySync("2025/11/03");
-  // dailySync("2025/11/04");
-  // dailySync("2025/11/05");
-  // dailySync("2025/11/06");
-  // dailySync("2025/11/07");
 }
