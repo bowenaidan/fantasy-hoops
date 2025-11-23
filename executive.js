0f63ed2f6843bb39a9fde85c8a28162b278d176b
Aidan Bowen <95006701+bowenaidan@users.noreply.github.com>
2025-11-23 16:45:45 -0600

    cleanup executive

--- executive.js
+++ executive.js
@@ -1,9 +1,4 @@
 function updateRunner() {
-  const d = new Date();
-  d.setDate(d.getDate());
-  const yyyy = d.getFullYear();
-  const mm = String(d.getMonth() + 1).padStart(2, '0');
-  const dd = String(d.getDate()).padStart(2, '0');
-  isoDate = `${yyyy}/${mm}/${dd}`;
+  isoDate = getTodayIsoDate_();
   dailySync(isoDate);
 }
