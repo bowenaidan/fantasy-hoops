20c95f2a762b94624b95451134b038eb05c49d38
Aidan Bowen <95006701+bowenaidan@users.noreply.github.com>
2025-12-12 17:36:35 -0600

    add write table

--- tableUtils.js
+++ tableUtils.js
@@ -9,3 +9,17 @@ function readTable(sheetName) {
     return obj;
   });
 }
+
+function writeTable(sheetName, dataArray) {
+  if (dataArray.length === 0) return;
+
+  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName);
+
+  const headers = Object.keys(dataArray[0]);
+  const values = [headers].concat(
+    dataArray.map(obj => headers.map(h => obj[h]))
+  );
+
+  sheet.clearContents();
+  sheet.getRange(1,1,values.length,values[0].length).setValues(values);
+}
