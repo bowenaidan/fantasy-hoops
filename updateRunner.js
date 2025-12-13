4184081df2f6c7cfdbec966251a7c69ba3e1ba72
Aidan Bowen <95006701+bowenaidan@users.noreply.github.com>
2025-12-12 22:24:45 -0600

    day change at 3 am

--- updateRunner.js
+++ updateRunner.js
@@ -5,6 +5,8 @@ function updateRunner() {
 
 function getTodayIsoDate_() {
   const d = new Date();
+  d.setHours(d.getHours() - 3);
+  Logger.log(d);
   const yyyy = d.getFullYear();
   const mm = String(d.getMonth() + 1).padStart(2, '0');
   const dd = String(d.getDate()).padStart(2, '0');
