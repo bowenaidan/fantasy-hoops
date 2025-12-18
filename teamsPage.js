bef1de8740720a1ad34ec77c8d09690de622b893
Aidan Bowen <95006701+bowenaidan@users.noreply.github.com>
2025-12-18 00:38:57 -0600

    feat: add daily score to manager score

--- teamsPage.js
+++ teamsPage.js
@@ -6,14 +6,28 @@ function doGet() {
 function getTeamsPageData() {
   const teams = readTable(SHEET_TEAMS);
   const managerTotalsMap = new Map();
+  const managerDailyTotalMap = new Map();
 
   teams.forEach(team => {
     const manager = team?.manager || 'Unassigned';
     const points = Number(team?.points);
+    const todayPoints = Number(team?.points_today);
+    if (!isNaN(todayPoints)) {
+      managerDailyTotalMap.set(manager, (managerDailyTotalMap.get(manager || 0) || 0) + todayPoints);
+    }
     if (!isNaN(points)) {
       managerTotalsMap.set(manager, (managerTotalsMap.get(manager) || 0) + points);
     }
   });
+
+  const managerDailyTotal = Array.from(managerDailyTotalMap.entries())
+    .map(([manager, dailyPoints]) => ({ manager, dailyPoints }))
+    .sort((a, b) => {
+      if (b.dailyPoints === a.dailyPoints) {
+        return a.manager.localeCompare(b.manager);
+      }
+      return b.dailyPoints - a.dailyPoints;
+    });
 
   const managerTotals = Array.from(managerTotalsMap.entries())
     .map(([manager, totalPoints]) => ({ manager, totalPoints }))
@@ -27,5 +41,6 @@ function getTeamsPageData() {
   return {
     teams,
     managerTotals,
+    managerDailyTotal, 
   };
 }
